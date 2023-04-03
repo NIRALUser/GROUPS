1f9b904ad1c2d1a533ac74fd9e5168a5494e9b02
David Allemang <david.allemang@kitware.com>
2023-04-03 12:37:14 -0400

    Revert "COMP: Upgrade SurfRemesh"
    This reverts commit a1f6f9edc391ead32bf4ed2b6706034d5e057f41.

--- SuperBuild/External_SurfRemesh.cmake
+++ SuperBuild/External_SurfRemesh.cmake
@@ -33,7 +33,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "4393744a0e0f7a01a28b41704fb35397daf6284d" # master (2023-04-03)
+    "36f99985d82e9133ef2ab56cb061e061423a1d2a" # master (2023-04-03)
     QUIET
     )
 
